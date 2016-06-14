4dafdd93566ac07e74d148e282afb2e49295c767
rdelvallej32 <rdelvallej@gmail.com>
2016-06-13 22:33:54 -0400

    Add attributes to contact model
    - set properties of a contact
    - set relationship of contact to user

--- app/contact/model.js
+++ app/contact/model.js
@@ -1,7 +1,15 @@
 import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
-
+  first_name: attr('string'),
+  last_name: attr('string'),
+  occupation: attr('string'),
+  professional_relationship: attr('string'),
+  company: attr('string'),
+  last_contacted: attr('date'),
+  fact: attr('string'),
+  reminder: attr('number'),
+  user: belongsTo('user', { inverse: 'contacts' })
 });
