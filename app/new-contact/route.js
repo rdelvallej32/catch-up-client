61b3dca97995a86778a979296016b61909841bcd
rdelvallej32 <rdelvallej@gmail.com>
2016-06-15 10:14:19 -0400

    Add createContact action
    - Add createContact action to test if the action works when user submits
    a form

--- app/new-contact/route.js
+++ app/new-contact/route.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
+
+  actions: {
+    createContact(data) {
+      console.log(data);
+      console.log("clicked");
+    }
+
+  },
+
 });
