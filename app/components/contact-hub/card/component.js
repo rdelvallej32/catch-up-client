0568e43797e55eed7ac99266297f21ca14eb4494
rdelvallej32 <rdelvallej@gmail.com>
2016-06-15 14:52:22 -0400

    Add delete action in contacts page
    - Add delete button in contact-hub component template
    - Add action handler to destroyRecord when it is successful

--- app/components/contact-hub/card/component.js
+++ app/components/contact-hub/card/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    deleteContact() {
+      console.log('clicked');
+      this.get('contact').destroyRecord();
+    }
+  }
 });
