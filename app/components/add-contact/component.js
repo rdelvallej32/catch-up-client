6d5af5e76e27a98d034282effb0ebb52e44c85c8
rdelvallej32 <rdelvallej@gmail.com>
2016-06-15 09:33:03 -0400

    Add component templates for each input
    - bound the value of each data type to the input

--- app/components/add-contact/component.js
+++ app/components/add-contact/component.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'form',
+  classNames: ['form-horizontal'],
+  auth: Ember.inject.service(),
+
+  contact: {},
+
+  actions: {
+    submit () {
+      this.sendAction('createContact', this.get('contact'));
+    },
+
+    reset () {
+      this.set('contact', {});
+      this.sendAction('goHome');
+    },
+  },
 });
