75b5b597a55ab2bee48e1e9fa9325e7180c27832
rdelvallej32 <rdelvallej@gmail.com>
2016-06-16 21:05:20 -0400

    Add editable content in contact hub
    - Add actions to save, cancel and toggle between editing or not
    - Add to template input values for items to change
    - Can edit, but need to save it to database

--- app/components/contact-hub/component.js
+++ app/components/contact-hub/component.js
@@ -1,6 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  contenteditable: false,
+  firstName: Ember.computed(function() {
+    return this.get('contact.first_name');
+  }),
+  lastName: Ember.computed(function() {
+    return this.get('contact.last_name');
+  }),
+  occupation: Ember.computed(function() {
+    return this.get('contact.occupation');
+  }),
+  company: Ember.computed(function() {
+    return this.get('contact.company');
+  }),
+  fact: Ember.computed(function() {
+    return this.get('contact.fact');
+  }),
+  lastContacted: Ember.computed(function() {
+    return this.get('contact.last_contacted');
+  }),
+  reminder: Ember.computed(function() {
+    return this.get('contact.reminder');
+  }),
 
-  
+  contact: {},
+  cancel: function() {
+    this.set('contenteditable', false);
+  },
+
+  edit: function() {
+    this.set('contenteditable', true);
+  },
+
+  save: function() {
+    // this.set('contenteditable', false);
+    // this.set('contact.first_name', this.get('contact.firstName'));
+
+  },
+
+
 });
