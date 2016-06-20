e852b16fa32dca2bf944596e6a9dfac58e0d4124
rdelvallej32 <rdelvallej@gmail.com>
2016-06-20 14:10:41 -0400

    Fix card bug
    - Fix bug that prevented card from expanding the length of the window
    - Add images folder for landing page image

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // activate: function() {
+  //     $('body').css('background', "url('images/catchup.png') no-repeat center center fixed");
+  //     $('body').css('background-size', 'cover');
+  //     Ember.$('body').append()
+  //
+  //     },
+  //     deactivate: function() {
+  //       $('body').css('background', 'none');
+  //       $('body').remove('background');
+  //     }
 });
